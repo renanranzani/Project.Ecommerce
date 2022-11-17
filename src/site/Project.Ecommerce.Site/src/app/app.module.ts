43f409a919f85486ae087bcd7f5fe3af269d080b
Renan Rodrigues Ranzani <rranzani@viceri.com.br>
2022-11-17 10:45:51 -0300

    Requisições Postman e criação de Componentes e Services

--- src/site/Project.Ecommerce.Site/src/app/app.module.ts
+++ src/site/Project.Ecommerce.Site/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './Registration/registration/registration.component';
+import { DetailsComponent } from './Details/details/details.component';
+import { GridComponent } from './Grid/grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    DetailsComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule
